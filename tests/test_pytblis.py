c14757ce28c2562f3a46a29f0ea2a264e4fed1f6
chillenb <chillenbrand15@gmail.com>
2025-07-24 13:15:23 -0400

    import test

---
+++ tests/test_pytblis.py
@@ -0,0 +1,8 @@
+import sys
+import pytest
+
+import pytblis
+
+def test_pytblis_imported():
+    """Sample test, will always pass so long as import statement worked."""
+    assert "pytblis" in sys.modules
