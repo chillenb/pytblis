0dde290341456c1a4c7fb5e647bc346db7e09b9e
chillenb <chillenbrand15@gmail.com>
2025-07-26 02:02:06 -0400

    does this work

--- scripts/cibw_before_all.sh
+++ scripts/cibw_before_all.sh
@@ -14,5 +14,3 @@ cmake -S tblis -B tblisbld \
   -DBLIS_CONFIG_FAMILY=${PYTBLIS_ARCH}
 cmake --build tblisbld --parallel 8
 cmake --install tblisbld
-
-export CMAKE_PREFIX_PATH=${PROJECT_DIR}/tblisprefix
