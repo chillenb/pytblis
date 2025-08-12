9abd0e53e0c794833a183983150266552a8fbc75
chillenb <chillenbrand15@gmail.com>
2025-08-11 22:19:41 -0400

    update threading config

--- scripts/cibw_before_all.sh
+++ scripts/cibw_before_all.sh
@@ -11,14 +11,20 @@ if [[ "${PLATFORM_ID}" == "macosx_x86_64" ]]; then
   export CFLAGS="-arch x86_64"
   export CXXFLAGS="-arch x86_64"
   export GA_NCPU=4
+  export THREAD_MODEL=pthreads
+  export HWLOC_ENABLED=OFF
 fi
 
 if [[ "${PLATFORM_ID}" == "macosx_arm64" ]]; then
   export GA_NCPU=3
+  export THREAD_MODEL=pthreads
+  export HWLOC_ENABLED=OFF
 fi
 
 if [[ "${PLATFORM_ID}" == manylinux* ]]; then
   export GA_NCPU=4
+  export THREAD_MODEL=openmp
+  export HWLOC_ENABLED=ON
   dnf -y install hwloc hwloc-devel
 fi
 
@@ -28,7 +34,9 @@ cmake -S tblis -B tblisbld \
   -DCMAKE_C_COMPILER="${C_COMPILER}" \
   -DCMAKE_CXX_COMPILER="${CXX_COMPILER}" \
   -DBLIS_CONFIG_FAMILY="${PYTBLIS_ARCH}" \
-  -DENABLE_HWLOC=ON
+  -DENABLE_HWLOC="${HWLOC_ENABLED}" \
+  -DBLIS_THREAD_MODEL="${THREAD_MODEL}" \
+  -DENABLE_THREAD_MODEL="${THREAD_MODEL}"
 # cmake --build tblisbld --parallel "${GA_NCPU}" --verbose
 make -C tblisbld -j "${GA_NCPU}"
 cmake --install tblisbld
